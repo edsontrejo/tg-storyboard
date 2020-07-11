d3a81018057c5999af0c803ec22eb121ebfb4720
Edson Trejo <edsontrejohdz@gmail.com>
2020-07-11 10:01:44 -0500

    Se sube al repositorio inicial

---
+++ tg-rating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tg-rating
+//
+//  Created by Edson Trejo on 7/11/20.
+//  Copyright © 2020 Edson Trejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
